6ca41203e9dcb7d528800c6914ee4a020c36164a
ExploiD2005 <exploid2005@gmail.com>
2019-10-08 23:22:22 +1100

    Добавили AddNoteActivity

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -5,14 +5,16 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerViewNotes;
-    private ArrayList<Note> notes = new ArrayList<>();
+    public static final ArrayList<Note> notes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +33,12 @@ public class MainActivity extends AppCompatActivity {
         //recyclerViewNotes.setLayoutManager(new GridLayoutManager(this, 3));
         //recyclerViewNotes.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         recyclerViewNotes.setAdapter(adapter);
+
+    }
+
+    public void onClickAddNote(View view){
+        Intent intent = new Intent(this, AddNoteActivity.class);
+        startActivity(intent);
 
     }
 }
