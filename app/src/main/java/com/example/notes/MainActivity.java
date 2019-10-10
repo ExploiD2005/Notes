de197058b984827ebd63bc7da69e4b1976594ecd
ExploiD2005 <exploid2005@gmail.com>
2019-10-10 15:10:35 +1100

    Исправили ошибки

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -21,13 +21,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerViewNotes = findViewById(R.id.recyclerViewNotes);
-        notes.add(new Note("Парикмахер", "Сделать прическу", "Понедельник", 2));
-        notes.add(new Note("Баскетбол", "Игра со школьной командой", "Вторник", 3));
-        notes.add(new Note("Магазин", "Купить новые джинсы", "Понедельник", 3));
-        notes.add(new Note("Стоматолог", "Вылечить зубы", "Понедельник", 2));
-        notes.add(new Note("Парикмахер", "Сделать прическу к выпускному", "Среда", 1));
-        notes.add(new Note("Баскетбол", "Игра со школьной командой", "Вторник", 3));
-        notes.add(new Note("Магазин", "Купить новые джинсы", "Понедельник", 3));
+        if (notes.isEmpty()) {
+            notes.add(new Note("Парикмахер", "Сделать прическу", "Понедельник", 2));
+            notes.add(new Note("Баскетбол", "Игра со школьной командой", "Вторник", 3));
+            notes.add(new Note("Магазин", "Купить новые джинсы", "Понедельник", 3));
+            notes.add(new Note("Стоматолог", "Вылечить зубы", "Понедельник", 2));
+            notes.add(new Note("Парикмахер", "Сделать прическу к выпускному", "Среда", 1));
+            notes.add(new Note("Баскетбол", "Игра со школьной командой", "Вторник", 3));
+            notes.add(new Note("Магазин", "Купить новые джинсы", "Понедельник", 3));
+        }
         NotesAdapter adapter = new NotesAdapter(notes);
         recyclerViewNotes.setLayoutManager(new LinearLayoutManager(this));
         //recyclerViewNotes.setLayoutManager(new GridLayoutManager(this, 3));
